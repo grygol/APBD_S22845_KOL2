e227e38e819c13acc927da20d36b9b0b537cbdee
michal <s22845@pjwstk.edu.pl>
2022-06-14 13:47:11 +0200

    IdMusicalbum nullable

--- Entities/Models/Track.cs
+++ Entities/Models/Track.cs
@@ -8,7 +8,7 @@ namespace kol2.Entities.Models
         public int IdTrack { get; set; }
         public string TrackName { get; set; }
         public float Duration { get; set; }
-        public int IdMusicAlbum { get; set; }
+        public int? IdMusicAlbum { get; set; }
         public virtual IEnumerable<Musician_Track> Musician_Tracks  { get; set; }
         public virtual Album Album { get; set; }
     }
---
+++ Migrations/20220614114542_added nullable music album.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace kol2.Migrations
+{
+    public partial class addednullablemusicalbum : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "IdMusicAlbum",
+                table: "Track",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.UpdateData(
+                table: "Album",
+                keyColumn: "IdAlbum",
+                keyValue: 1,
+                column: "PublishDate",
+                value: new DateTime(2022, 6, 14, 13, 45, 41, 924, DateTimeKind.Local).AddTicks(8720));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "IdMusicAlbum",
+                table: "Track",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Album",
+                keyColumn: "IdAlbum",
+                keyValue: 1,
+                column: "PublishDate",
+                value: new DateTime(2022, 6, 14, 13, 29, 57, 274, DateTimeKind.Local).AddTicks(1330));
+        }
+    }
+}
